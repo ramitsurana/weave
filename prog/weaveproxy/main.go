dc320472f125990c8c5f33850ec3ac8896c534a8
Paul Bellamy <paul.a.bellamy@gmail.com>
2015-07-07 14:34:37 +0100

    [proxy] autoconfigure based on docker daemon's cli flags

--- prog/weaveproxy/main.go
+++ prog/weaveproxy/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"net"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/docker/docker/pkg/mflag"
@@ -21,8 +26,12 @@ type listOpts struct {
 }
 
 func ListVar(p *[]string, names []string, value []string, usage string) {
+	FlagSetListVar(mflag.CommandLine, p, names, value, usage)
+}
+
+func FlagSetListVar(flagset *mflag.FlagSet, p *[]string, names []string, value []string, usage string) {
 	*p = value
-	mflag.Var(&listOpts{p, false}, names, usage)
+	flagset.Var(&listOpts{p, false}, names, usage)
 }
 
 func (opts *listOpts) Set(value string) error {
@@ -48,16 +57,20 @@ func main() {
 
 	c.Version = version
 
+	if err := loadDockerDefaults(&c); err != nil {
+		Log.Fatalf("Error loading default arguments from docker: %s", err)
+	}
+
 	mflag.BoolVar(&justVersion, []string{"#version", "-version"}, false, "print version and exit")
 	mflag.StringVar(&logLevel, []string{"-log-level"}, "info", "logging level (debug, info, warning, error)")
-	ListVar(&c.ListenAddrs, []string{"H"}, defaultListenAddrs, "addresses on which to listen")
+	ListVar(&c.ListenAddrs, []string{"H", "-host"}, c.ListenAddrs, "addresses on which to listen")
 	mflag.BoolVar(&c.NoDefaultIPAM, []string{"#-no-default-ipam", "-no-default-ipalloc"}, false, "do not automatically allocate addresses for containers without a WEAVE_CIDR")
 	mflag.BoolVar(&c.NoRewriteHosts, []string{"no-rewrite-hosts"}, false, "do not automatically rewrite /etc/hosts. Use if you need the docker IP to remain in /etc/hosts")
-	mflag.StringVar(&c.TLSConfig.CACert, []string{"#tlscacert", "-tlscacert"}, "", "Trust certs signed only by this CA")
-	mflag.StringVar(&c.TLSConfig.Cert, []string{"#tlscert", "-tlscert"}, "", "Path to TLS certificate file")
-	mflag.BoolVar(&c.TLSConfig.Enabled, []string{"#tls", "-tls"}, false, "Use TLS; implied by --tls-verify")
-	mflag.StringVar(&c.TLSConfig.Key, []string{"#tlskey", "-tlskey"}, "", "Path to TLS key file")
-	mflag.BoolVar(&c.TLSConfig.Verify, []string{"#tlsverify", "-tlsverify"}, false, "Use TLS and verify the remote")
+	mflag.StringVar(&c.TLSConfig.CACert, []string{"#tlscacert", "-tlscacert"}, c.TLSConfig.CACert, "Trust certs signed only by this CA")
+	mflag.StringVar(&c.TLSConfig.Cert, []string{"#tlscert", "-tlscert"}, c.TLSConfig.Cert, "Path to TLS certificate file")
+	mflag.BoolVar(&c.TLSConfig.Enabled, []string{"#tls", "-tls"}, c.TLSConfig.Enabled, "Use TLS; implied by --tls-verify")
+	mflag.StringVar(&c.TLSConfig.Key, []string{"#tlskey", "-tlskey"}, c.TLSConfig.Key, "Path to TLS key file")
+	mflag.BoolVar(&c.TLSConfig.Verify, []string{"#tlsverify", "-tlsverify"}, c.TLSConfig.Verify, "Use TLS and verify the remote")
 	mflag.BoolVar(&c.WithDNS, []string{"-with-dns", "w"}, false, "instruct created containers to always use weaveDNS as their nameserver")
 	mflag.BoolVar(&c.WithoutDNS, []string{"-without-dns"}, false, "instruct created containers to never use weaveDNS as their nameserver")
 	mflag.Parse()
@@ -82,4 +95,116 @@ func main() {
 	}
 
 	p.ListenAndServe()
+}
+
+func loadDockerDefaults(c *proxy.Config) error {
+	procfs := os.Getenv("PROCFS")
+	// find the docker PID
+	statusFile, err := os.Open(filepath.Join(procfs, "self/status"))
+	if err != nil {
+		return err
+	}
+	defer statusFile.Close()
+	dockerPID := ""
+	scanner := bufio.NewScanner(statusFile)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		if len(fields) == 2 && fields[0] == "PPid:" {
+			dockerPID = fields[1]
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	if err := parseDockerEnv(procfs, dockerPID); err != nil {
+		return err
+	}
+
+	argBytes, err := ioutil.ReadFile(filepath.Join(procfs, dockerPID, "cmdline"))
+	if err != nil {
+		return err
+	}
+
+	args := nullTermToStrings(argBytes)[1:]
+
+	//filter down to only the flags we expect, so parsing
+	//them doesn't bail
+	filteredArgs := []string{}
+	for i := 0; i < len(args); i++ {
+		arg := strings.SplitN(args[i], "=", 2)
+		switch arg[0] {
+		case "--tls":
+			filteredArgs = append(filteredArgs, args[i])
+		case "--tlsverify", "--tlscacert", "--tlscert",
+			"--tlskey", "-H", "--host":
+			filteredArgs = append(filteredArgs, args[i])
+			if !strings.Contains(args[i], "=") {
+				i++
+				if i < len(args) {
+					filteredArgs = append(filteredArgs, args[i])
+				}
+			}
+		}
+	}
+
+	dockerFlags := &mflag.FlagSet{}
+	dockerFlags.BoolVar(&c.TLSConfig.Enabled, []string{"-tls"}, false, "")
+	dockerFlags.BoolVar(&c.TLSConfig.Verify, []string{"-tlsverify"}, false, "")
+	dockerFlags.StringVar(&c.TLSConfig.CACert, []string{"-tlscacert"}, "", "")
+	dockerFlags.StringVar(&c.TLSConfig.Cert, []string{"-tlscert"}, "", "")
+	dockerFlags.StringVar(&c.TLSConfig.Key, []string{"-tlskey"}, "", "")
+	FlagSetListVar(dockerFlags, &c.ListenAddrs, []string{"H", "-host"}, defaultListenAddrs, "")
+	if err := dockerFlags.Parse(filteredArgs); err != nil {
+		return err
+	}
+
+	newAddrs := []string{}
+	for _, addr := range c.ListenAddrs {
+		if strings.HasPrefix(addr, "unix://") {
+			addr = strings.Replace(addr, "docker", "weave", -1)
+		} else {
+			host, _, err := net.SplitHostPort(strings.TrimPrefix(addr, "tcp://"))
+			if err != nil {
+				return err
+			}
+			addr = fmt.Sprintf("tcp://%s:12375", host)
+		}
+		newAddrs = append(newAddrs, addr)
+	}
+	c.ListenAddrs = newAddrs
+
+	return nil
+}
+
+func parseDockerEnv(procfs, dockerPID string) error {
+	envBytes, err := ioutil.ReadFile(filepath.Join(procfs, dockerPID, "environ"))
+	if err != nil {
+		return err
+	}
+	options := []string{"DOCKER_CERT_PATH", "DOCKER_TLS_VERIFY"}
+	for _, line := range nullTermToStrings(envBytes) {
+		for _, option := range options {
+			prefix := fmt.Sprint(option, "=")
+			if strings.HasPrefix(line, prefix) {
+				os.Setenv(option, strings.TrimPrefix(line, prefix))
+			}
+		}
+	}
+	return nil
+}
+
+func nullTermToStrings(b []byte) []string {
+
+	strs := []string{}
+	for {
+		i := bytes.IndexByte(b, 0)
+		if i == -1 {
+			break
+		}
+		strs = append(strs, string(b[0:i]))
+		b = b[i+1:]
+	}
+	return strs
 }
